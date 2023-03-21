6eda912b8b384654dfbd131842074b6ef17ed08f
fzc <1378799690@qq.com>
2023-03-21 19:31:28 +0800

    无关紧要

--- Test.java
+++ Test.java
@@ -1,9 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Test {
     public static void main(String[] args) {
         Character[] charArray = {'G', 'e', 'e', 'k', 's'};
