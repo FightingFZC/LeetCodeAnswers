9a06e79cf57d8cb0a999798dc18a49a9ff705dbb
fzc <1378799690@qq.com>
2023-03-25 23:46:23 +0800

    完成第8题

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Long.MAX_VALUE);
+        System.out.println((int)'0');
 
         System.out.println(Long.valueOf("-8463847412"));
     }
